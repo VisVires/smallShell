891c0bead0814e479cc8bb1d946ce04fca422c9b
VisVires <wgeorge@outlook.com>
2017-03-27 08:14:07 -0700

    finished arrayds

--- HackerRank/cs/arrayds.cs
+++ HackerRank/cs/arrayds.cs
@@ -5,21 +5,27 @@ using System.Linq;
 class Solution {
 
     static void Main(String[] args) {
+        //number reader
         string n;
+        string[] arr_temp;
+        //get first number
         var fileStream = new FileStream(@"array.txt", FileMode.Open, FileAccess.Read);
+        //get numbers from file
         using (var streamReader = new StreamReader(fileStream))
         {
             n = streamReader.ReadLine();
-            string[] arr_temp = streamReader.ReadLine().Split(' ');
+            arr_temp = streamReader.ReadLine().Split(' ');
         }
+        //convert original number to an int
         int num = Convert.ToInt32(n);
-        Console.Write(num.ToString());
-        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
-        printInReverse(arr);
+        Console.Write(num.ToString()+ "\n");
+        //int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        printInReverse(arr_temp);
     }
    
-    static void printInReverse(int[] arr){
-        for(int i = 0; i < arr.Length; i++){
+    //function to print the original input in reverse
+    static void printInReverse(string[] arr){
+        for(int i = arr.Length - 1; i >= 0; i--){
             Console.Write(arr[i] + " ");
         }
     }   
