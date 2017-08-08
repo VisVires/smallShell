0b34e47c7add449e71db8a251c84eca853f63ebf
georgew <wgeorge@outlook.com>
2017-08-07 21:24:21 -0700

    acmp

--- BookExercises/HackerRank/cpp/acmIcpc.cpp
+++ BookExercises/HackerRank/cpp/acmIcpc.cpp
@@ -13,11 +13,10 @@ int main() {
     myfile.open("array.txt");
 
 
-    int numOfPeople; 
-    int numOfTopics;
-    myfile >> numOfPeople >> numOfTopics;
+    unsigned int numOfPeople, tokens; 
+    myfile >> numOfPeople >> tokens;
 
-    const int nOT = 5;
+    //const int nOT = makeConst(numOfTopics);
     //const size_t size = numOfTopics;
 
     vector<string> people;
@@ -27,11 +26,48 @@ int main() {
         people.push_back(person);
     }
 
-    bitset<nOT> a(people[0]), b(people[1]);
-    bitset<nOT> c = a | b;
+    vector<string> results;
 
-    cout << c << endl;
+    int maxTopics = 0;
+    for (auto i = 0u; i < people.size()/2+1; i++){
+        string string1 = people.at(i);
+        for(auto j = people.size()-1; j > people.size()/2; j--){ 
+            string string2 = people.at(j);
+            int ones = 0;
+            string result = "";
+            for(auto k = 0u; k < tokens; k++){
+                if(string1[k] == '1' || string2[k] == '1'){
+                    result += '1';
+                    ones++;
+                    if (ones >= maxTopics){
+                        maxTopics = ones;
+                    }
+                } else {
+                    result += '0';
+                }
+            }
+            results.push_back(result);
+        }
+        //cout << endl;
+    }
 
+    int totalTeams = 0;
+    for(auto i = 0u; i < results.size(); i++){
+        int count = 0;
+        string word = results[i];
+        for (auto j = 0u; j < word.size(); j++){
+            if(word[j] == '1'){
+                count++;
+                if (count == maxTopics){
+                    totalTeams++;
+                }
+            }
+        }
+    }
+    
+    cout << maxTopics << endl;
+    cout << totalTeams << endl;
     return 0;
 }
+
 
