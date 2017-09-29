a76a6c5498cd5046e4cae1f50940eeb9031df2f8
georgew <wgeorge@outlook.com>
2017-09-29 09:49:04 -0700

    added linux study files to git

--- BookExercises/leetcode/cpp/intToEng.cpp
+++ BookExercises/leetcode/cpp/intToEng.cpp
@@ -8,7 +8,7 @@
 #include <stack>
 using namespace std;
 
-string addSuffix(int commas){
+  string addSuffix(int commas){
     switch(commas){
             case 1: return "Thousand ";
                     break;
@@ -21,98 +21,106 @@ string addSuffix(int commas){
 }
 
 
-string hunderedToWords(deque<int> hundered, int commas){
-    
-    string w = "";
-    string space = " ";
+    string hunderedToWords(deque<int> hundered, int commas){
 
-        
-    string ones [20] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifeteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    
-    string tens [8] = { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}; 
-    
-    if(hundered.size() > 2 && hundered[0] != 0){
-        w += ones[hundered[0]-1] + " Hundered ";
-    } else if (hundered.size() < 3){
-        while(hundered.size() < 3){
-            hundered.push_front(0);
+        string w = "";
+        string space = " ";
+
+
+        string ones [20] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+        string tens [8] = { "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}; 
+
+        if(hundered.size() > 2 && hundered[0] != 0){
+            w += ones[hundered[0]-1] + " Hundred";
+        } else if (hundered.size() < 3){
+            while(hundered.size() < 3){
+                hundered.push_front(0);
+            }
         }
-    }
-    
-    int num = (10 * hundered[1]) + hundered[2];
-    if(num == 0){
+
+        int num = (10 * hundered[1]) + hundered[2];
+        if(num == 0){
+            w += space + addSuffix(commas);
+            return w;
+        }
+        
+        if(w != "")
+            w += space;
+        
+        
+        if(hundered[1] > 1){
+            if(hundered[2] != 0)
+                w += tens[hundered[1]-2] + space + ones[hundered[2] - 1];
+            else
+               w += tens[hundered[1]-2];
+        } else {
+                w += ones[num-1];            
+        }
+        
+        w += space + addSuffix(commas);
+        
+
         return w;
     }
 
-    if(hundered[1] > 1){
-        w += tens[hundered[1]-2] + space + ones[hundered[2] - 1];
-    } else {
-        if(w != ""){
-            w += space + ones[num-1];
-        } else {
-            w = ones[num-1];
+
+    string numberToWords(int num){
+        int commas;
+        string number = to_string(num);
+
+        unsigned int numLength = number.length();
+        deque<int> n;
+
+        if(num == 0){
+            return "Zero";
         }
-    } 
-    w += space + addSuffix(commas);  
 
-    return w;
-}
-
-
-string numberToWords(int num){
-    int commas;
-    string number = to_string(num);
-    
-    unsigned int numLength = number.length();
-    deque<int> n;
-
-    if(num == 0){
-        return "Zero";
-    }
-
-    //stack it up
-    for(auto i = 0u; i < numLength; i++){
-       int c = number.at(i) - '0';
-       n.push_front(c);
-    }
-
-    //cout << n.front() << endl;
-    string finalNum = "";
-
-
-    if(numLength == 10){
-        commas = 3;        
-    } else if (numLength >= 7 && numLength < 10){
-        commas = 2;
-    } else if (numLength >= 4 && numLength < 7){
-        commas = 1;
-    } else {
-        commas = 0;
-    }
-
-    deque<int> hundered;
-    unsigned int remainder = n.size();
-    
-    while (commas >= 0){
-        hundered.clear();
-        if(n.size() > 3){
-            remainder = n.size() - (3 * commas);
-        } else {
-            remainder = n.size();
+        //stack it up
+        for(auto i = 0u; i < numLength; i++){
+           int c = number.at(i) - '0';
+           n.push_front(c);
         }
-        
-        for(auto i = 0u; i < remainder; i++){
-            if(!n.empty()){
-                hundered.push_back(n.back());
-                n.pop_back();
+
+        //cout << n.front() << endl;
+        string finalNum = "";
+
+
+        if(numLength == 10){
+            commas = 3;        
+        } else if (numLength >= 7 && numLength < 10){
+            commas = 2;
+        } else if (numLength >= 4 && numLength < 7){
+            commas = 1;
+        } else {
+            commas = 0;
+        }
+
+        deque<int> hundered;
+        unsigned int remainder = n.size();
+
+        while (commas >= 0){
+            hundered.clear();
+            if(n.size() > 3){
+                remainder = n.size() - (3 * commas);
+            } else {
+                remainder = n.size();
             }
-        }
-        finalNum += hunderedToWords(hundered, commas);
-        commas--;
-    }
 
+            for(auto i = 0u; i < remainder; i++){
+                if(!n.empty()){
+                    hundered.push_back(n.back());
+                    n.pop_back();
+                }
+            }
+     
+            finalNum += hunderedToWords(hundered, commas);
+            commas--;
+        }
         
-    return finalNum;
+       finalNum.erase(std::find_if(finalNum.rbegin(), finalNum.rend(), std::bind1st(std::not_equal_to<char>(), ' ')).base(), finalNum.end());
+
+        return finalNum;
 }
 
 
