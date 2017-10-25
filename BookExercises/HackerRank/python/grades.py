8b866cc3201f5d85cc1459f994a0edf8c5ce1444
georgew <wgeorge@outlook.com>
2017-10-25 09:55:30 -0700

    more hackerrank python work and linux OS boot info

--- BookExercises/HackerRank/python/grades.py
+++ BookExercises/HackerRank/python/grades.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+
+def getNewGrade(grade):
+    if grade < 38:
+        return grade
+    if grade % 5 > 2:
+        return grade + (5-(grade % 5))
+    else:
+        return grade
+
+
+def solve(grades):
+    newGrades = []
+    for grade in grades:
+        newGrades.append(getNewGrade(grade))
+    return newGrades
+        
+
 with open("array.txt", "r") as f:
     data = f.readlines()
 
@@ -10,5 +27,7 @@ index = 1
 grades = []
 for item in data[1:]:
     grades.append(int(item))
-print inputs
-print grades
+result = solve(grades)
+
+print '/n'.join(map(str, result))
+
